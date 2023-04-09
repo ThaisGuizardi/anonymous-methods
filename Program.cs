1e084ebd1144d83fec4ec399f184000df585ef8d
Thais Guizardi <thaisguizardi@hotmail.com>
2023-04-09 11:54:22 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Anonymous Methods
+MeasurementConverter converter = delegate (string measureFrom, string measuresTo, double valueFrom)
+{
+    double valueConverted = 0.0D;
+
+    if (measureFrom == "m" && measuresTo == "cm")
+    {
+        valueConverted = valueFrom * 100;
+    }
+    else if (measureFrom == "cm" && measuresTo == "m")
+    {
+        valueConverted = valueFrom / 100;
+    }
+
+    Console.WriteLine($"Anonymous method: {valueFrom} {measureFrom} to {measuresTo} = {valueConverted} {measuresTo}");
+};
+
+// Lambda Expression
+MeasurementConverter converterLambdaExpression = (string measureFrom, string measuresTo, double valueFrom) =>
+{
+    double valueConverted = 0.0D;
+
+    if (measureFrom == "m" && measuresTo == "cm")
+    {
+        valueConverted = valueFrom * 100;
+    }
+    else if (measureFrom == "cm" && measuresTo == "m")
+    {
+        valueConverted = valueFrom / 100;
+    }
+
+    Console.WriteLine($"Lambda Expression: {valueFrom} {measureFrom} to {measuresTo} = {valueConverted} {measuresTo}");
+};
+
+converter("m", "cm", 10);
+
+Console.WriteLine("\n");
+
+converterLambdaExpression("cm", "m", 10);
+
+Console.Read();
+
+delegate void MeasurementConverter(string measureFrom, string measuresTo, double valueFrom);
